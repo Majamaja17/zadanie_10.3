afd8dd4df0c59720c0e912b6efe313af66ef5832
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-17 19:02:36 +0200

    Change js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(function(){
 
-	var carouselList = $('#carousel ul');
+	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
 
 	function changeSlide() {
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		carouselList.animate({"marginLeft":-400}, 500, moveFirstSlide);
 	}
 
 	function moveFirstSlide() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem)
 		carouselList.css({marginLeft:0});
 	}
